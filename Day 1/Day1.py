82d2934e7361888379f283d4364fba1db121859d
Torsteen1 <143719073+Torsteen1@users.noreply.github.com>
2024-12-01 13:55:45 +0100

    Day 1 solutions

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,30 +1,21 @@
-from typing import List  
-
+from collections import Counter
 
 def import_data(filename):
     with open(filename, 'r') as file:
-        list1, list2 = [], []
-        for line in file:
-            num1, num2 = map(int, line.split())
-            list1.append(num1)
-            list2.append(num2)
-    return list1, list2
-filename = '/Users/torsten/Desktop/AoC-2024/Day 1/input.txt'
+        return zip(*[map(int, line.split()) for line in file])
+    
+filename = 'Day 1/input.txt'
 list1, list2 = import_data(filename) 
 
-
 def calculate_distance(list1, list2): 
+  return sum(abs(a - b) for a, b in zip(sorted(list1), sorted(list2)))
 
-    sorted_list1 = sorted(list1)
-    sorted_list2 = sorted(list2)
-    total_distance = 0
+def similiraity_score(list1, list2):
+    frequency_dict = Counter(list2)
+    return sum(num * frequency_dict.get(num, 0) for num in list1)
 
-    for num1, num2 in zip(sorted_list1, sorted_list2):
-        total_distance += abs(num1 - num2)
-
-    return total_distance
-
-print(calculate_distance(list1, list2))  
+print("Similarity score:", similiraity_score(list1, list2))
+print("Total Distance:" , calculate_distance(list1, list2))  
 
 
 
